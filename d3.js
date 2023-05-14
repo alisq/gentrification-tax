6442fc68954fbd2e9679549b9243e2bc44ef1cac
alisq <ali.shamas.qadeer@gmail.com>
2023-05-14 13:15:10 -0400

    basic bar chart operational.

--- d3.js
+++ d3.js
@@ -172,3 +172,115 @@ svg.append("text")
   .text("Year purchased");
 
 
+
+
+
+  ///STACKED BAR CHART
+  sampleData = [
+    { label: 'Property Tax', value: 4.65 },
+    { label: 'Municipal Lang and Transfer Tax', value: 0.94 },
+    { label: 'Development Charges', value: 0.38 },
+    { label: 'Section 37 Community Benefits', value: 0.08 },
+    { label: 'Vacant Home Tax', value: 0.08 },
+    { label: 'Other Tax Supported Revenues', value: 2.91 },
+    { label: 'Rate Supported Revenues', value: 1.96 },
+    { label: 'Federal and Provincial Subsidies', value: 4.07 },
+  ]
+
+  stackedBar(sampleData, {
+    colors: ['#ff616b', '#fa9442', '#bf36e0','#ff616b', '#fa9442', '#bf36e0','#ff616b', '#fa9442', '#bf36e0']
+  })
+
+  function stackedBar (data, {
+    height = 400,
+    barHeight = 200,
+    halfBarHeight = barHeight / 2,
+    f = d3.format('.1f'),
+    margin = {top: 20, right: 0, bottom: 20, left: 0},
+    w = 1300,
+    h = height * 0.66,
+    colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3", "#ff7f00", "#ffff33"]
+  } = {}) {
+  
+    // Have a total of values for reference from the data:
+    const total = d3.sum(data, d => d.value);
+    console.info('total', total);
+  
+    // Format the data (instead of using d3.stack()) and filter out 0 values:
+    function groupDataFunc(data) {
+      // use a scale to get percentage values
+      const percent = d3.scaleLinear()
+        .domain([0, total])
+        .range([0, 100])
+      // filter out data that has zero values
+      // also get mapping for next placement
+      // (save having to format data for d3 stack)
+      let cumulative = 0
+      const _data = data.map(d => {
+        cumulative += d.value
+        return {
+          value: d.value,
+          // want the cumulative to prior value (start of rect)
+          cumulative: cumulative - d.value,
+          label: d.label,
+          percent: percent(d.value)
+        }
+      }).filter(d => d.value > 0)
+      return _data
+    };
+  
+    const groupData = groupDataFunc(data);
+    console.info('groupData', groupData);
+  
+    const svg_stacked = d3.select("#bar_svg")
+    .append("svg").attr("viewBox", `0 0 ${w} ${height}`)
+
+    const sel = d3.select(svg_stacked);
+    
+    // set up scales for horizontal placement
+    const xScale = d3.scaleLinear()
+      .domain([0, total])
+      .range([0, w]);
+  
+    const join = svg_stacked.selectAll('g')
+      .data(groupData)
+      .join('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+  
+    // stack rect for each data value
+    join.append('rect')
+      .attr('class', 'rect-stacked')
+      .attr('x', d => xScale(d.cumulative))
+      .attr('y', h / 2 - halfBarHeight)
+      .attr('height', barHeight)
+      .attr('width', d => xScale(d.value))
+      .style('fill', (d, i) => colors[i])
+      .style('stroke','black')
+  
+    // add values on bar
+    join.append('text')
+      .attr('class', 'text-value')
+      .attr('text-anchor', 'middle')
+      .attr('x', d => xScale(d.cumulative) + (xScale(d.value) / 2))
+      .attr('y', (h / 2) + 5)
+      .text(d => d.value);
+  
+    // add some labels for percentages
+    join.append('text')
+      .attr('class', 'text-percent')
+      .attr('text-anchor', 'middle')
+      .attr('x', d => xScale(d.cumulative) + (xScale(d.value) / 2))
+      .attr('y', (h / 2) - (halfBarHeight * 1.1))
+      .text(d => f(d.percent) + ' %');
+  
+    // add the labels
+    join.append('text')
+      .attr('class', 'text-label')
+      .attr('text-anchor', 'middle')
+      .attr('x', d => xScale(d.cumulative) + (xScale(d.value) / 2))
+      .attr('y', (h / 2) + (halfBarHeight * 1.1) + 20)
+      .style('fill', (d, i) => colors[i])
+      .text(d => d.label);
+    
+    return svg_stacked;
+  }
